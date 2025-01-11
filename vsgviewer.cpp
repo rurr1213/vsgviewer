8892fd316bb2da7714296c6ece32c5619637cd9d
ravi <rurr1213@gmail.com>
2025-01-10 18:09:38 -0800

    saves screen shot as a png

--- vsgviewer.cpp
+++ vsgviewer.cpp
@@ -1,13 +1,272 @@
 #include <vsg/all.h>
 
-#ifdef vsgXchange_FOUND
-#    include <vsgXchange/all.h>
-#endif
+//#ifdef vsgXchange_FOUND
+#include <vsgXchange/all.h>
+//#endif
 
+#include <cstdio>
+#include <cstdlib>
 #include <algorithm>
 #include <chrono>
 #include <iostream>
 #include <thread>
+
+vsg::ref_ptr<vsg::Data> captureScreenshot(vsg::ref_ptr<vsg::Window> window, vsg::ref_ptr<vsg::Options> options, vsg::ref_ptr<vsg::Event> event, bool eventDebugTest = false) // Add event and eventDebugTest parameters
+{
+    // printInfo(window);
+
+    if (eventDebugTest && event && event->status() == VK_EVENT_RESET)
+    {
+        std::cout << "event->status() == VK_EVENT_RESET" << std::endl;
+        // manually wait for the event to be signaled
+        while (event->status() == VK_EVENT_RESET)
+        {
+            std::cout << "w";
+            std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        }
+        std::cout << std::endl;
+    }
+
+    auto width = window->extent2D().width;
+    auto height = window->extent2D().height;
+
+    auto device = window->getDevice();
+    auto physicalDevice = window->getPhysicalDevice();
+    auto swapchain = window->getSwapchain();
+
+    // get the colour buffer image of the previous rendered frame as the current frame hasn't been rendered yet.  The 1 in window->imageIndex(1) means image from 1 frame ago.
+    auto sourceImage = window->imageView(window->imageIndex(1))->image;
+
+    VkFormat sourceImageFormat = swapchain->getImageFormat();
+    VkFormat targetImageFormat = sourceImageFormat;
+
+    //
+    // 1) Check to see if Blit is supported.
+    //
+    VkFormatProperties srcFormatProperties;
+    vkGetPhysicalDeviceFormatProperties(*(physicalDevice), sourceImageFormat, &srcFormatProperties);
+
+    VkFormatProperties destFormatProperties;
+    vkGetPhysicalDeviceFormatProperties(*(physicalDevice), VK_FORMAT_R8G8B8A8_UNORM, &destFormatProperties);
+
+    bool supportsBlit = ((srcFormatProperties.optimalTilingFeatures & VK_FORMAT_FEATURE_BLIT_SRC_BIT) != 0) &&
+                        ((destFormatProperties.linearTilingFeatures & VK_FORMAT_FEATURE_BLIT_DST_BIT) != 0);
+
+    if (supportsBlit)
+    {
+        // we can automatically convert the image format when blit, so take advantage of it to ensure RGBA
+        targetImageFormat = VK_FORMAT_R8G8B8A8_UNORM;
+    }
+
+    vsg::info("supportsBlit = ", supportsBlit);
+
+    //
+    // 2) create image to write to
+    //
+    auto destinationImage = vsg::Image::create();
+    destinationImage->imageType = VK_IMAGE_TYPE_2D;
+    destinationImage->format = targetImageFormat;
+    destinationImage->extent.width = width;
+    destinationImage->extent.height = height;
+    destinationImage->extent.depth = 1;
+    destinationImage->arrayLayers = 1;
+    destinationImage->mipLevels = 1;
+    destinationImage->initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+    destinationImage->samples = VK_SAMPLE_COUNT_1_BIT;
+    destinationImage->tiling = VK_IMAGE_TILING_LINEAR;
+    destinationImage->usage = VK_IMAGE_USAGE_TRANSFER_DST_BIT;
+
+    destinationImage->compile(device);
+
+    auto deviceMemory = vsg::DeviceMemory::create(device, destinationImage->getMemoryRequirements(device->deviceID), VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT);
+
+    destinationImage->bind(deviceMemory, 0);
+
+    //
+    // 3) create command buffer and submit to graphics queue
+    //
+    auto commands = vsg::Commands::create();
+
+    if (event)
+    {
+        vsg::info("Using vsg::Event/vkEvent");
+        commands->addChild(vsg::WaitEvents::create(VK_PIPELINE_STAGE_BOTTOM_OF_PIPE_BIT, VK_PIPELINE_STAGE_BOTTOM_OF_PIPE_BIT, event));
+        commands->addChild(vsg::ResetEvent::create(event, VK_PIPELINE_STAGE_BOTTOM_OF_PIPE_BIT));
+    }
+
+    // 3.a) transition destinationImage to transfer destination initialLayout
+    auto transitionDestinationImageToDestinationLayoutBarrier = vsg::ImageMemoryBarrier::create(
+        0,                                                             // srcAccessMask
+        VK_ACCESS_TRANSFER_WRITE_BIT,                                  // dstAccessMask
+        VK_IMAGE_LAYOUT_UNDEFINED,                                     // oldLayout
+        VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,                          // newLayout
+        VK_QUEUE_FAMILY_IGNORED,                                       // srcQueueFamilyIndex
+        VK_QUEUE_FAMILY_IGNORED,                                       // dstQueueFamilyIndex
+        destinationImage,                                              // image
+        VkImageSubresourceRange{VK_IMAGE_ASPECT_COLOR_BIT, 0, 1, 0, 1} // subresourceRange
+    );
+
+    // 3.b) transition swapChainImage from present to transfer source initialLayout
+    auto transitionSourceImageToTransferSourceLayoutBarrier = vsg::ImageMemoryBarrier::create(
+        VK_ACCESS_MEMORY_READ_BIT,                                     // srcAccessMask
+        VK_ACCESS_TRANSFER_READ_BIT,                                   // dstAccessMask
+        VK_IMAGE_LAYOUT_PRESENT_SRC_KHR,                               // oldLayout
+        VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL,                          // newLayout
+        VK_QUEUE_FAMILY_IGNORED,                                       // srcQueueFamilyIndex
+        VK_QUEUE_FAMILY_IGNORED,                                       // dstQueueFamilyIndex
+        sourceImage,                                                   // image
+        VkImageSubresourceRange{VK_IMAGE_ASPECT_COLOR_BIT, 0, 1, 0, 1} // subresourceRange
+    );
+
+    auto cmd_transitionForTransferBarrier = vsg::PipelineBarrier::create(
+        VK_PIPELINE_STAGE_TRANSFER_BIT,                       // srcStageMask
+        VK_PIPELINE_STAGE_TRANSFER_BIT,                       // dstStageMask
+        0,                                                    // dependencyFlags
+        transitionDestinationImageToDestinationLayoutBarrier, // barrier
+        transitionSourceImageToTransferSourceLayoutBarrier    // barrier
+    );
+
+    commands->addChild(cmd_transitionForTransferBarrier);
+
+    if (supportsBlit)
+    {
+        // 3.c.1) if blit using vkCmdBlitImage
+        VkImageBlit region{};
+        region.srcSubresource.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+        region.srcSubresource.layerCount = 1;
+        region.srcOffsets[0] = VkOffset3D{0, 0, 0};
+        region.srcOffsets[1] = VkOffset3D{static_cast<int32_t>(width), static_cast<int32_t>(height), 1};
+        region.dstSubresource.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+        region.dstSubresource.layerCount = 1;
+        region.dstOffsets[0] = VkOffset3D{0, 0, 0};
+        region.dstOffsets[1] = VkOffset3D{static_cast<int32_t>(width), static_cast<int32_t>(height), 1};
+
+        auto blitImage = vsg::BlitImage::create();
+        blitImage->srcImage = sourceImage;
+        blitImage->srcImageLayout = VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL;
+        blitImage->dstImage = destinationImage;
+        blitImage->dstImageLayout = VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL;
+        blitImage->regions.push_back(region);
+        blitImage->filter = VK_FILTER_NEAREST;
+
+        commands->addChild(blitImage);
+    }
+    else
+    {
+        // 3.c.2) else use vkCmdCopyImage
+
+        VkImageCopy region{};
+        region.srcSubresource.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+        region.srcSubresource.layerCount = 1;
+        region.dstSubresource.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+        region.dstSubresource.layerCount = 1;
+        region.extent.width = width;
+        region.extent.height = height;
+        region.extent.depth = 1;
+
+        auto copyImage = vsg::CopyImage::create();
+        copyImage->srcImage = sourceImage;
+        copyImage->srcImageLayout = VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL;
+        copyImage->dstImage = destinationImage;
+        copyImage->dstImageLayout = VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL;
+        copyImage->regions.push_back(region);
+
+        commands->addChild(copyImage);
+    }
+
+    // 3.d) transition destination image from transfer destination layout to general layout to enable mapping to image DeviceMemory
+    auto transitionDestinationImageToMemoryReadBarrier = vsg::ImageMemoryBarrier::create(
+        VK_ACCESS_TRANSFER_WRITE_BIT,                                  // srcAccessMask
+        VK_ACCESS_MEMORY_READ_BIT,                                     // dstAccessMask
+        VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,                          // oldLayout
+        VK_IMAGE_LAYOUT_GENERAL,                                       // newLayout
+        VK_QUEUE_FAMILY_IGNORED,                                       // srcQueueFamilyIndex
+        VK_QUEUE_FAMILY_IGNORED,                                       // dstQueueFamilyIndex
+        destinationImage,                                              // image
+        VkImageSubresourceRange{VK_IMAGE_ASPECT_COLOR_BIT, 0, 1, 0, 1} // subresourceRange
+    );
+
+    // 3.e) transition swap chain image back to present
+    auto transitionSourceImageBackToPresentBarrier = vsg::ImageMemoryBarrier::create(
+        VK_ACCESS_TRANSFER_READ_BIT,                                   // srcAccessMask
+        VK_ACCESS_MEMORY_READ_BIT,                                     // dstAccessMask
+        VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL,                          // oldLayout
+        VK_IMAGE_LAYOUT_PRESENT_SRC_KHR,                               // newLayout
+        VK_QUEUE_FAMILY_IGNORED,                                       // srcQueueFamilyIndex
+        VK_QUEUE_FAMILY_IGNORED,                                       // dstQueueFamilyIndex
+        sourceImage,                                                   // image
+        VkImageSubresourceRange{VK_IMAGE_ASPECT_COLOR_BIT, 0, 1, 0, 1} // subresourceRange
+    );
+
+    auto cmd_transitionFromTransferBarrier = vsg::PipelineBarrier::create(
+        VK_PIPELINE_STAGE_TRANSFER_BIT,                // srcStageMask
+        VK_PIPELINE_STAGE_TRANSFER_BIT,                // dstStageMask
+        0,                                             // dependencyFlags
+        transitionDestinationImageToMemoryReadBarrier, // barrier
+        transitionSourceImageBackToPresentBarrier      // barrier
+    );
+
+    commands->addChild(cmd_transitionFromTransferBarrier);
+
+    auto fence = vsg::Fence::create(device);
+    auto queueFamilyIndex = physicalDevice->getQueueFamily(VK_QUEUE_GRAPHICS_BIT);
+    auto commandPool = vsg::CommandPool::create(device, queueFamilyIndex);
+    auto queue = device->getQueue(queueFamilyIndex);
+
+    vsg::submitCommandsToQueue(commandPool, fence, 100000000000, queue, [&](vsg::CommandBuffer& commandBuffer) {
+        commands->record(commandBuffer);
+    });
+
+    //
+    // 4) map image and copy
+    //
+    VkImageSubresource subResource{VK_IMAGE_ASPECT_COLOR_BIT, 0, 0};
+    VkSubresourceLayout subResourceLayout;
+    vkGetImageSubresourceLayout(*device, destinationImage->vk(device->deviceID), &subResource, &subResourceLayout);
+
+    size_t destRowWidth = width * sizeof(vsg::ubvec4);
+    vsg::ref_ptr<vsg::Data> imageData;
+    if (destRowWidth == subResourceLayout.rowPitch)
+    {
+        imageData = vsg::MappedData<vsg::ubvec4Array2D>::create(deviceMemory, subResourceLayout.offset, 0, vsg::Data::Properties{targetImageFormat}, width, height); // deviceMemory, offset, flags and dimensions
+    }
+    else
+    {
+        // Map the buffer memory and assign as a ubyteArray that will automatically unmap itself on destruction.
+        // A ubyteArray is used as the graphics buffer memory is not contiguous like vsg::Array2D, so map to a flat buffer first then copy to Array2D.
+        auto mappedData = vsg::MappedData<vsg::ubyteArray>::create(deviceMemory, subResourceLayout.offset, 0, vsg::Data::Properties{targetImageFormat}, subResourceLayout.rowPitch * height);
+        imageData = vsg::ubvec4Array2D::create(width, height, vsg::Data::Properties{targetImageFormat});
+        for (uint32_t row = 0; row < height; ++row)
+        {
+            std::memcpy(imageData->dataPointer(row * width), mappedData->dataPointer(row * subResourceLayout.rowPitch), destRowWidth);
+        }
+    }
+
+    return imageData; // Return the captured image data
+}
+
+void captureAndSave(vsg::ref_ptr<vsg::Window> _window, vsg::ref_ptr<vsg::Options> _options, vsg::ref_ptr<vsg::Event> _event)
+{
+    static int times = 20;
+
+    if (times--==0) {
+        if (auto imageData = captureScreenshot(_window, _options, _event))
+        {
+            vsg::Path filename = _options->paths.empty() ? "screenshot.png" : _options->paths[0] / "screenshot.png";
+//            vsg::Path filename = _options->paths.empty() ? "screenshot2.jpg" : _options->paths[0] / "screenshot2.jpg";
+//            std::remove(filename.c_str());
+            if (vsg::write(imageData, filename, _options))
+            {
+                std::cout << "Screenshot saved to " << filename << std::endl;
+            }
+            else
+            {
+                std::cout << "Failed to save screenshot." << std::endl;
+            }
+        }
+        times = 100;
+    }
+}
 
 vsg::ref_ptr<vsg::Node> createTextureQuad(vsg::ref_ptr<vsg::Data> sourceData, vsg::ref_ptr<vsg::Options> options)
 {
@@ -202,7 +461,10 @@ int main(int argc, char** argv)
             vsg::Path filename = arguments[i];
             path = vsg::filePath(filename);
 
+       std::cout << "openning file " + filename << std::endl;
+
             auto object = vsg::read(filename, options);
+
             if (auto node = object.cast<vsg::Node>())
             {
                 group->addChild(node);
@@ -357,6 +619,11 @@ int main(int argc, char** argv)
             }
         }
 
+        // Declare do_image_capture here, in the main function's scope
+        bool do_image_capture = false;
+        bool eventDebugTest = false; // or true if you need the debug behavior
+        vsg::ref_ptr<vsg::Event> event; //  Make sure this is declared if the screenshot function uses it.
+
         viewer->start_point() = vsg::clock::now();
 
         // rendering main loop
@@ -368,6 +635,8 @@ int main(int argc, char** argv)
             viewer->update();
 
             viewer->recordAndSubmit();
+
+            captureAndSave(window, options, event);
 
             viewer->present();
         }
